2a07b1c70103fae534a344f10d7cc7e7f7ed0b46
Joel George <mynameisjoelgeorge@gmail.com>
2016-09-09 16:47:23 +0530

    Add files via upload

---
+++ sgoc/sgoc/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+// 	$("#faith-section .fatih-subsection").hover(function(){
+// 			$("#faith-section .fatih-subsection .btn").css("opacity", 1.0).css("transition", "0.5s");
+// 		},
+// 	function(){
+// 		$("#faith-section .fatih-subsection .btn").css("opacity", 0.5).css("transition", "0.5s");
+// 	});
+// });
